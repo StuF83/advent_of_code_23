0c6461da498d4e6106d0864a08198a9822b94c7b
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-12-01 18:29:50 +0000

    day one part one complete

--- 01/day_one.rb
+++ 01/day_one.rb
@@ -11,3 +11,26 @@ test_input = ["1abc2",
 "pqr3stu8vwx",
 "a1b2c3d4e5f",
 "treb7uchet"]
+
+coordinate_numbers = []
+
+def coordinate_string_to_array(string)
+  string.split("")
+end
+
+def find_first_number(array)
+  array.each { | element| return element if (1..9).include?element.to_i }
+end
+
+def find_last_number(array)
+  find_first_number(array.reverse!)
+end
+
+input.each do | coordinate_string |
+  coordinate_array = coordinate_string_to_array(coordinate_string)
+  first_number = find_first_number(coordinate_array)
+  last_number = find_last_number(coordinate_array)
+  coordinate_numbers << first_number.concat(last_number).to_i
+end
+
+puts "The sum for the calibration document is #{coordinate_numbers.sum}"
