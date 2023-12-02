05c2a1a43c43b2300e19260839f428712490bad4
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-12-02 21:32:26 +0000

    day_two_part_one complete

--- 02/day_two_part_one.rb
+++ 02/day_two_part_one.rb
@@ -18,5 +18,37 @@ game_conditions = {
   "blue" => 14
 }
 
-bag_draws = []
-test_input.each { |line| bag_draws << line.split(": ")[1].split(";") }
+valid_games = []
+
+def game_string_to_array(string)
+  string.split(": ")[1].split(";").map(&:strip)
+end
+
+def draw_to_hash(string)
+  game = string.split(",").map(&:strip)
+  draw_hash = {}
+  game.each do | draw |
+    number = draw.match (/\d+/)
+    colour = draw.match (/red|blue|green/)
+    draw_hash[colour[0]] = number[0].to_i
+  end
+  draw_hash
+end
+
+def valid_game?(game, game_conditions)
+  game.each do | draw |
+    draw.each do | key, value |
+      return false if value > game_conditions[key]
+    end
+  end
+  true
+end
+
+input.each_with_index do | game, index |
+  game_array = game_string_to_array(game)
+  game = game_array.map { | game | draw_to_hash(game)}
+  # p "Game #{index + 1}"
+  valid_games << (index + 1) if valid_game?(game, game_conditions)
+end
+
+p valid_games.sum
