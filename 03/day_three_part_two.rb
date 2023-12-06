950d1dcf847e88d1040e776de46586e5a4e0744a
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-12-06 21:08:52 +0000

    horizontal matches working to return anumber before and/or after symbol into and array

--- 03/day_three_part_two.rb
+++ 03/day_three_part_two.rb
@@ -6,10 +6,10 @@ input = Nokogiri::HTML5(URI.open("input.html")).text
 input = input.split("\n")
 
 # test data
-test_input = ["....411...............838......721.....44..............................................607..................................................",
-"...&......519..................*..........#.97.........994..............404..............*...&43........440...882.......673.505.............",
+test_input = [
+"...&......519................55*..........#.97.........994..............404..............*.22&43........440...882......673.505.............",
 ".....*......*...892.........971...%....131....*..........*.......515...$.......157.....412.............-.....*.............*............594.",
-"..856.495....13...-...............602..........36...$.985....341*.........88.....*.921....................122..................806..508....."]
+"..856.495....13...-...............602..........36..1$.985....341*.........88.....*.921....................122..................806..508....."]
 
 # test_input = ["467..114..",
 # "123*22....",
@@ -36,20 +36,34 @@ test_input = ["....411...............838......721.....44........................
 # Below the grid such that x is [x-1, x, x+1] and y is y-1. If there is no digit, we can move onto the line below. If there is a
 
 
-def horizontal_matches(string, array)
-  positions = string.enum_for(:scan, /([^a-zA-Z\d\n.])/).map { Regexp.last_match.begin(0) }
-  if positions.any?
-    string = string.split("")
-    before = string.shift(positions[0]).reverse.join.match(/\d+/)
-    after = string.join.match(/(?<=[^a-zA-Z.\d])\d+/)
+def horizontal_matches(string, position)
+  array = []
+  string = string.split("")
+  index_before = string[position - 1]
+  index_after = string[position + 1]
+
+  if index_before =~ /\d/
+    before = string.shift(position).reverse.join.match(/\d+/)
     array << before[0].reverse if before
-    array << after[0].reverse if after
   end
-  array
+
+  if index_after =~ /\d/
+    after = string.join.match(/(?<=[^a-zA-Z.\d])\d+/)
+    array << after[0] if after
+  end
+
+  p array
 end
 
 test_input.each do |line|
-  adjacent_numbers = []
-  adjacent_numbers = horizontal_matches(line, adjacent_numbers)
-  p adjacent_numbers
+  symbol_positions = line.enum_for(:scan, /([^a-zA-Z\d\n.])/).map { Regexp.last_match.begin(0) }
+  if symbol_positions.any?
+    symbol_positions.each do |position|
+      array = horizontal_matches(line, position)
+    end
+  end
+
+  # adjacent_numbers = []
+  # adjacent_numbers = horizontal_matches(line, adjacent_numbers)
+  # p adjacent_numbers
 end
