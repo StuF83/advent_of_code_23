93d2e767e29b2fc12e2ce9d07652e66a83df4872
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-12-09 18:09:49 +0000

    day 4 complete, not optimised pass - takes 20 plus seconds

--- 04/day_four_part_one.rb
+++ 04/day_four_part_one.rb
@@ -2,17 +2,17 @@ require 'nokogiri'
 require 'open-uri'
 
 # input data
-# input = Nokogiri::HTML5(URI.open("input.html")).text
-# input = input.split("\n")
+input = Nokogiri::HTML5(URI.open("input.html")).text
+input = input.split("\n")
 
-input = <<~INPUT
-Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-INPUT
+# input = <<~INPUT
+# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+# INPUT
 
 # p input
 
@@ -54,14 +54,13 @@ class Card
   def bonus_cards
     self.score
     cards = self.winning_numbers_count? >= 1 ? Range.new(@id + 1, (@id + self.winning_numbers_count?)).to_a : []
-    p cards
   end
 end
 
 card_pile = Hash.new
 card_pile.default = 0
 
-input.each_line do | line |
+input.each do | line |
   card = Card.parse(line)
   card_pile[card.id] += 1
   card_pile[card.id].times do
@@ -75,3 +74,4 @@ card_pile.each_value { | value | total_cards += value}
 p total_cards
 
 # p total.sum
+# 1219459 is too low
