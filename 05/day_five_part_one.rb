2eb428f445e364ded0994fd6fcc515a9da95fa65
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-12-09 20:19:46 +0000

    day five setup complete

---
+++ 05/day_five_part_one.rb
@@ -0,0 +1,53 @@
+require 'nokogiri'
+require 'open-uri'
+
+t0 = Time.now
+
+# input data
+# input = Nokogiri::HTML5(URI.open("input.html")).text
+# input = input.split("\n")
+
+input = <<~INPUT
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+INPUT
+
+input = input.split("\n\n").map { |string| string.split("\n")}
+
+
+
+
+
+
+puts Time.now - t0
